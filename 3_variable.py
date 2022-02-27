f0d053475cdd767d781102ac909d92bd87ee2119
Fiixabdal <abdabd59@kingshigh.school.nz>
2022-02-28 09:43:48 +1300

    completed

---
+++ 3_variable.py
@@ -0,0 +1,42 @@
+#1
+player_lives"3"
+#2
+player_username"Abdul"
+#3
+plyar_money"0"
+#4
+player_number="22 031 9908"
+#5
+user_name = "Abdul"   
+#6
+user_age = "16"
+#7
+user_year = "11"
+#8
+print("user_name")
+#9
+print("user_age")
+#10
+print("user_year")
+#11
+user_username = "Abdalabdul"   
+#12
+user_school = "king_high_school"
+#13
+user_favourite = "burger"
+#14
+user_pet = "dont have"
+#15
+user_chocolate_ate = "2"
+#16
+player_score = "0"
+#17
+player_highscore = "10000"
+#18
+player_laps = "2"
+#19
+player_ammo = "1000"
+#20
+player_gear = "4"
+#21
+player_speed = "120"
