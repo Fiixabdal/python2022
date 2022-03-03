b27091e2ca062897f768824e6c394335f1606c4c
Fiixabdal <abdabd59@kingshigh.school.nz>
2022-03-03 13:21:21 +1300

    Update 5_input.py

---
+++ 5_input.py
@@ -0,0 +1,19 @@
+#1
+("push enter")
+#2
+("type in your name and push enter")
+#3
+("type in your age and push enter")
+#4
+user = input("what is your name")
+#5
+age = input ("how old are you")
+#6
+fav_movie = input("what is your fav movie")
+#7
+book = input("what is your fav book")
+#8
+adjective = input("write a adjective")
+#9
+noun = input("write a noun")
+
